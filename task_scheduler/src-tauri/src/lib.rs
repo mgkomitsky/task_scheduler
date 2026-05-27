f6cca786bc24576e37ded12f00af115a5b00c7a4
Matthew Komitsky <mgkomitsky@gmail.com>
2026-05-27 18:47:48 -0500

    Learning how to annotate Rust functions and call from tsx

--- task_scheduler/src-tauri/src/lib.rs
+++ task_scheduler/src-tauri/src/lib.rs
@@ -4,11 +4,18 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+#[tauri::command]
+fn test(){
+    println!("HI!!!!");
+}
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, test])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
