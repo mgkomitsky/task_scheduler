fdb9dfbe288320a228b2182c9aaa6a9203d2b2c1
Matthew Komitsky <mgkomitsky@gmail.com>
2026-05-27 21:23:31 -0500

    Added more parameters to the parser

--- task_scheduler/src-tauri/src/parser.rs
+++ task_scheduler/src-tauri/src/parser.rs
@@ -4,6 +4,19 @@ use serde::Deserialize;
 pub struct Task {
     id: String,
     title: String,
+    tasktype: String,
+    status: String,
+    priority: String,
+    created: Option<String>,
+    due: Option<String>,
+    ended: Option<String>,
+    depends_on: Vec<String>,
+    tags: Vec<String>,
+    general_status: String,
+    blocker: String,
+    risk: String,
+    ask: String,
+    outcome: String,
 }
 
 pub fn parse_item(file_content: &str) -> Task {
